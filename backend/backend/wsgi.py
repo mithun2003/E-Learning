1fa492d2facd6926616eac0c4084871e6cd13ce1
Mithun Thomas <mithunc.y65@gmail.com>
2023-06-08 09:02:55 +0530

    wsgi update

--- backend/backend/wsgi.py
+++ backend/backend/wsgi.py
@@ -8,13 +8,11 @@ https://docs.djangoproject.com/en/4.2/howto/deployment/wsgi/
 """
 
 import os
-from static_ranges import Ranges
 
-from dj_static import Cling,MediaCling
 
 from django.core.wsgi import get_wsgi_application
 
 os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'backend.settings')
 
-# application = get_wsgi_application()
-application = Ranges(Cling(MediaCling(get_wsgi_application())))
+application = get_wsgi_application()
+# application = Ranges(Cling(MediaCling(get_wsgi_application())))
