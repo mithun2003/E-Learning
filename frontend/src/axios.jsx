08a2564df00b2d479676ed8678c037303e1298f9
Mithun Thomas <63800900+mithun2003@users.noreply.github.com>
2023-06-09 20:37:58 +0530

    Update axios.jsx

--- frontend/src/axios.jsx
+++ frontend/src/axios.jsx
@@ -88,7 +88,8 @@
 import axios from "axios";
 
 const instance = axios.create({
-  baseURL: 'http://localhost:8000',
+//   baseURL: 'http://localhost:8000',
+  baseURL: 'http://44.202.145.227/',
   headers: {
     'Content-Type': 'application/json',
     Accept: 'application/json'
