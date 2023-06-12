ea2e227d6e5482c381d4fa003db2f5f70103e55b
Mithun Thomas <mithunc.y65@gmail.com>
2023-06-12 14:20:35 +0530

    changes after hosting

--- backend/backend/settings.py
+++ backend/backend/settings.py
@@ -29,13 +29,13 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # DEBUG = True
 SECRET_KEY = env('SECRET_KEY')
 DEBUG = env('DEBUG')
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['*']
 
 
 # Application definition
 
 INSTALLED_APPS = [
-    # 'daphne',
+    'daphne',
     'channels',
     'django.contrib.admin',
     'django.contrib.auth',
@@ -167,11 +167,8 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/4.2/howto/static-files/
 
-STATIC_URL = 'static/'
-STATICFILES_DIRS = [
-    os.path.join(BASE_DIR, 'build/static')
-]
-STATIC_ROOT = os.path.join(BASE_DIR, 'static')
+STATIC_URL = "/static/"
+STATIC_ROOT = os.path.join(BASE_DIR, "static")# Media files settings
 
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
 MEDIA_URL = '/media/'
@@ -226,8 +223,11 @@ DJOSER = {
 }
 AUTH_USER_MODEL = 'account.UserAccount'
 CORS_ORIGIN_ALLOW_ALL = True
-# CORS_ALLOW_CREDENTIALS = True
-
+CORS_ALLOW_CREDENTIALS = True
+if DEBUG:
+    CSRF_TRUSTED_ORIGINS = ['http://localhost:8000']
+else:
+    CSRF_TRUSTED_ORIGINS = ['https://e-learning003.netlify.app']
 # CORS_ALLOW_HEADERS = [
 #     'content-type',
 #     'x-auth-token',
